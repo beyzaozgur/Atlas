c78089181f3130ff0827df946f4ad69e1be14fe2
beyzaozgur <beyzaozgur35@hotmail.com>
2023-08-07 14:57:18 +0300

    contact page is added

---
+++ CoreDemo/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class ContactController : Controller
+	{
+
+		ContactManager contactManager = new ContactManager(new EfContactRepository());
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(Contact contact)
+		{
+			contact.ContactDate = DateTime.Now;
+			contact.ContactStatus = true;
+			contactManager.AddContact(contact);
+			return RedirectToAction("Index", "Blog");
+
+		}
+	}
+}
