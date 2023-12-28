9d0dab5c02471cd1d730bafccc59f4580b8f210f
beyzaozgur <beyzaozgur35@hotmail.com>
2023-12-28 23:07:17 +0300

    Add all notifications list page and messages table

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -30,6 +30,8 @@ namespace DataAccessLayer.Concrete
 		public DbSet<NewsLetter> NewsLetters { get; set; }
 		public DbSet<BlogRating> BlogRatings { get; set; }
 		public DbSet<Notification> Notifications { get; set; }
+		public DbSet<Message> Messages { get; set; }
+
 
 	}
 }
