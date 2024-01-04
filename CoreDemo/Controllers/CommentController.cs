9e11f33bf27130bc1a3105e59ab5b83449002db3
beyzaozgur <beyzaozgur35@hotmail.com>
2024-01-04 23:19:48 +0300

    Fix comment add partial view

--- CoreDemo/Controllers/CommentController.cs
+++ CoreDemo/Controllers/CommentController.cs
@@ -24,7 +24,7 @@ namespace CoreDemo.Controllers
         {
 			comment.CommentDate = DateTime.Now;
 			comment.CommentStatus = true;
-			comment.BlogID = 2;
+			//comment.BlogID = 2;
 			commentManager.TAdd(comment);
             return Json(new { result = true });
         }
