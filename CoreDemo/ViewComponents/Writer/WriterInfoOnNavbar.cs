71b866c86c0f49378f41f5f3f4ac65350501d20d
beyzaozgur <beyzaozgur35@hotmail.com>
2024-01-06 15:21:24 +0300

    Modify writer layout UI

---
+++ CoreDemo/ViewComponents/Writer/WriterInfoOnNavbar.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Writer
+{
+    public class WriterInfoOnNavbar : ViewComponent
+    {
+        WriterManager writerManager = new WriterManager(new EfWriterRepository());
+        public IViewComponentResult Invoke()
+        {
+            var writer = writerManager.GetById(1);
+            return View(writer);
+        }
+    }
+}
