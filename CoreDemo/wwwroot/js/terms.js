66c99f910c9d283e416ad122ced2f56d52360faa
beyzaozgur <beyzaozgur35@hotmail.com>
2024-01-23 22:35:03 +0300

    Add terms and conditions pop up

---
+++ CoreDemo/wwwroot/js/terms.js
@@ -0,0 +1,39 @@
+function getTermsAndConditions() {
+
+	const termsAndConditions = `
+										<br/>Welcome to Atlas! <br/><br/> <div class="text-left">Before you proceed with the registration process, please take a moment to review our terms and conditions carefully. By registering on our blog website, you agree to comply with and be bound by the following terms: <br/><br/>
+
+										<b>1. Acceptance of Terms:</b><br/>
+						   By registering on Atlas, you acknowledge that you have read, understood, and agree to be bound by these terms and conditions. If you do not agree with any part of these terms, please do not proceed with the registration process.<br/><br/>
+
+										<b>2. User Eligibility:</b><br/>
+				   You must be at least 13 years old to register on Atlas. By registering, you confirm that you meet this age requirement.<br/><br/>
+
+										<b>3. User Account:</b><br/>
+				   You are responsible for maintaining the confidentiality of your account credentials (username and password). You agree to notify us immediately of any unauthorized use of your account or any other security breach.<br/><br/>
+
+										<b>4. User Content:</b><br/>
+						   By posting content on Atlas, you grant us a non-exclusive, royalty-free, worldwide license to use, display, reproduce, and distribute your content. You are solely responsible for the content you post, and it must comply with our community guidelines.<br/><br/>
+
+										<b>5. Prohibited Activities:</b><br/>
+				   You agree not to engage in any unlawful, harmful, or disruptive activities on Atlas. Prohibited activities include, but are not limited to, spamming, hacking, and violating intellectual property rights.<br/><br/>
+
+										<b>6. Privacy Policy:</b><br/>
+				   Your use of Atlas is also governed by our Privacy Policy, which outlines how we collect, use, and disclose your personal information. Please review our Privacy Policy to understand our practices.<br/><br/>
+
+										<b>7. Termination of Account:</b><br/>
+				   We reserve the right to suspend or terminate your account at our discretion, without prior notice, if you violate these terms and conditions or engage in any activities that may harm Atlas or its users.<br/><br/>
+
+										<b>8. Changes to Terms:</b><br/>
+				   We may update these terms and conditions from time to time. It is your responsibility to review them periodically. Continued use of Atlas after changes constitutes acceptance of the modified terms.<br/><br/>
+
+										<b>9. Contact Information:</b><br/>
+				   If you have any questions or concerns about these terms and conditions, please contact us at team@atlas.com.<br/><br/>
+
+				Thank you for being a part of Atlas! We hope you enjoy your experience.<br/><br/>
+
+				Atlas Team</div>
+		`;
+
+	return termsAndConditions;
+}
